6820d9bab1a3439cd996d27dbfd1165137b28fb4
Sharinant <scharin.an@yandex.ru>
2021-04-23 23:10:27 +0300

    Add parse
    Add getting and parsing information to collectionView

--- MusicExpress/Managers/APICaller.swift
+++ MusicExpress/Managers/APICaller.swift
@@ -15,4 +15,29 @@ final class APICaller {
     public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
         
     }
+    
+    public func getTopSongs(success: @escaping (Data) -> Void, failure: @escaping (String) -> Void) {
+        
+        guard let url = URL(string: "https://musicexpress.sarafa2n.ru/api/v1/albums?count=6&from=0") else {
+            return
+        }
+        let urlRequest = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
+            if error != nil {
+                failure("error")
+                return
+            }
+           
+            guard let data = data else {
+                failure("empry data")
+                return
+            }
+            
+           
+            DispatchQueue.main.async {
+                success(data)
+            }
+        }
+        task.resume()
+    }
 }
